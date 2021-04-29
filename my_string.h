35df8c1fcafdeef8fd91c9d890a4dbcbb084330c
jxsminengo <83432911+jxsminengo@users.noreply.github.com>
2021-04-29 18:50:05 -0400

    Create my_string.h

---
+++ my_string.h
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#ifndef MY_STRING_H
+#define MY_STRING_H
+#include "generic.h"
+#include "status.h"
+
+typedef void* MY_STRING;
+
+MY_STRING my_string_init_default(void);
+
+MY_STRING my_string_init_c_string(const char* c_string);
+
+int my_string_get_capacity(MY_STRING hMy_string);
+
+int my_string_get_size(MY_STRING hMy_string);
+
+int my_string_compare(MY_STRING hLeft_string, MY_STRING hRight_string);
+
+Status my_string_extraction(MY_STRING hMy_string, FILE* fp);
+
+Status my_string_insertion(MY_STRING hMy_string, FILE* fp);
+
+Status my_string_push_back(MY_STRING hMy_string, char item);
+
+Status my_string_pop_back(MY_STRING hMy_string);
+
+char* my_string_at(MY_STRING hMy_string, int index);
+
+char* my_string_c_str(MY_STRING hMy_string);
+
+Status my_string_concat(MY_STRING hResult, MY_STRING hAppend);
+
+Boolean my_string_empty(MY_STRING hMy_string);
+
+//Status my_string_assignment(Item* pLeft,Item Right);                                                                                                                                                      
+
+void my_string_assignment(ITEM* pLeft, ITEM Right);
+
+void my_string_destroy(ITEM* pItem);
+
+Status get_word_key_value(MY_STRING current_word_family, MY_STRING new_key, MY_STRING word, char guess);
+
+Boolean guessed_already(MY_STRING guess_list, char c);
+
+#endif
